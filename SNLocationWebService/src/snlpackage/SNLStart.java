6cfc71c54d5023812582ff1729ec3fda1e97577e
shaffooo <shafiq.tnoli@gmail.com>
2014-09-03 19:25:14 -0500

    Created entry point of SNL Webservice: SNLStart.java

---
+++ SNLocationWebService/src/snlpackage/SNLStart.java
@@ -0,0 +1,20 @@
+package snlpackage;
+/**
+ * 
+ */
+
+/**
+ * @author ShafiqUr
+ *
+ */
+public class SNLStart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Starting Point of Web Service...");
+	}
+
+}
